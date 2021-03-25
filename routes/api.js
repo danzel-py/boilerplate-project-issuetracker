b9564ca5bf19ec4afbfac1c0481dd7834cf2d267
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-03-25 16:08:34 +0700

    .PUT is good

--- routes/api.js
+++ routes/api.js
@@ -2,63 +2,128 @@
 
 const ObjectID = require('mongodb').ObjectID;
 
-module.exports = function (app,collection) {
+module.exports = function (app, collection) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+
+    .get(function (req, res) {
       let project_name = req.params.project;
-      collection.findOne({project_name: project_name},(err,project)=>{
+      collection.findOne({ project_name: project_name }, (err, project) => {
         if (err) return console.log(err)
         if (!project) res.send([])
         res.send(project.issues)
       })
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project_name = 'apitest'
-      /* collection.findOne({project_name: project_name},(err, project)=>{
-        if (err) return console.log(err)
-        if (!project){
-          collection.insertOne({project_name: project_name, issues: []})
-        }
-        let newIssue = {
-          _id: new ObjectID(),
-          issue_title: req.body.issue_title,
-          issue_text: req.body.issue_text,
-          created_by: req.body.created_by,
-          ... (req.body.assigned_to && {assigned_to: req.body.assigned_to}),
-          ... (req.body.status_text && {status_text: req.body.status_text})
-        }
-        
-      }) */
       let newIssue = {
         _id: new ObjectID(),
         issue_title: req.body.issue_title,
         issue_text: req.body.issue_text,
         created_by: req.body.created_by,
-        ... (req.body.assigned_to && {assigned_to: req.body.assigned_to}),
-        ... (req.body.status_text && {status_text: req.body.status_text})
+        ... (req.body.assigned_to && { assigned_to: req.body.assigned_to }),
+        ... (req.body.status_text && { status_text: req.body.status_text }),
+        open: true,
+        created_on: new Date(),
+        updated_on: new Date()
       }
-      collection.updateOne({project_name: project_name},
-        {$push: {issues: newIssue}})
-    })
-    /*
-    _id
-      issue_title
-      issue_text
-      created_by
-      assigned_to
-      status_text
-      */
-    .put(function (req, res){
-      let project = req.params.project;
-      
+      collection.updateOne({ project_name: project_name },
+        { $push: { issues: newIssue } })
     })
     
-    .delete(function (req, res){
+    .put(function (req, res) {
+      let project_name = 'apitest';
+
+      collection.findOne({ project_name: project_name }, (err, project) => {
+        if (err) return console.log(err)
+        let selectedIssues = project.issues
+
+        // WHEN ID IS VALID
+        if (selectedIssues.find(x => x._id == req.body._id)) {
+
+          // OPTIONAL UPDATE
+          if (req.body.issue_title) {
+            collection.updateOne({ project_name: project_name }, {
+              $set: {
+                "issues.$[issue].issue_title": req.body.issue_title
+              }
+            }, {
+              arrayFilters: [{ "issue._id": ObjectID(req.body._id) }]
+            })
+          }
+          if (req.body.issue_text) {
+            collection.updateOne({ project_name: project_name }, {
+              $set: {
+                "issues.$[issue].issue_text": req.body.issue_text
+              }
+            }, {
+              arrayFilters: [{ "issue._id": ObjectID(req.body._id) }]
+            })
+          }
+          if (req.body.created_by) {
+            collection.updateOne({ project_name: project_name }, {
+              $set: {
+                "issues.$[issue].created_by": req.body.created_by
+              }
+            }, {
+              arrayFilters: [{ "issue._id": ObjectID(req.body._id) }]
+            })
+          }
+          if (req.body.assigned_to) {
+            collection.updateOne({ project_name: project_name }, {
+              $set: {
+                "issues.$[issue].assigned_to": req.body.assigned_to
+              }
+            }, {
+              arrayFilters: [{ "issue._id": ObjectID(req.body._id) }]
+            })
+          }
+          if (req.body.status_text) {
+            collection.updateOne({ project_name: project_name }, {
+              $set: {
+                "issues.$[issue].status_text": req.body.status_text
+              }
+            }, {
+              arrayFilters: [{ "issue._id": ObjectID(req.body._id) }]
+            })
+          }
+          if (req.body.open) {
+            collection.updateOne({ project_name: project_name }, {
+              $set: {
+                "issues.$[issue].open": false
+              }
+            }, {
+              arrayFilters: [{ "issue._id": ObjectID(req.body._id) }]
+            })
+          }
+
+          // IF ANYTHING GETS UPDATED
+          if (req.body.issue_title || req.body.issue_text || req.body.created_by || req.body.assigned_to || req.body.status_text || req.body.open) {
+            collection.updateOne({ project_name: project_name }, {
+              $set: {
+                "issues.$[issue].updated_on": new Date()
+              }
+            }, {
+              arrayFilters: [{ "issue._id": ObjectID(req.body._id) }]
+            })
+            res.send({
+              result: 'successfully updated',
+              _id: req.body._id
+            })
+          }
+        }
+
+        // WHEN ID IS INVALID
+        else {
+          res.send('invalid id')
+        }
+      })
+    })
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
+
     });
-    
+
 };
