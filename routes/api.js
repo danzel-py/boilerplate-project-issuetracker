73096b0366d11adf4edc68de1fb449148c8883c7
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-03-25 09:48:34 +0700

    Connection OK

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,17 @@
 'use strict';
 
-module.exports = function (app) {
+
+module.exports = function (app,collection) {
 
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      let project = req.params.project;
+      let project_name = req.params.project;
+      collection.findOne({project_name: project_name},(err,project)=>{
+        if (err) return console.log(err)
+        if (!project) res.send([])
+        res.send(project.issues)
+      })
       
     })
     
--- server.js
+++ server.js
@@ -18,33 +18,46 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/:project/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/issue.html');
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.DB;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  if(err) return console.log(err)
+  const collection = client.db("issuetrackerDB").collection("projects");
+
+  //Sample front-end
+  app.route('/:project/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/issue.html');
+    });
+  
+  //Index page (static HTML)
+  app.route('/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/index.html');
+     
+    });
+  
+  //For FCC testing purposes
+  fccTestingRoutes(app);
+  
+  //Routing for API 
+  apiRoutes(app,collection);  
+      
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
-
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
 });
+
+
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
